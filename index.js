fd1a90a04830da95ebc5430e4f3ebe19b6ab13ac
Collins <collowmartial@gmail.com>
2025-04-01 20:39:16 +0300

    modified text files

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 
-
-//declare drivers
-const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
-
-//returnFirstTwoDrivers() — Declare a variable with const that is assigned an anonymous function. The assigned function should accept an array of drivers as an argument and return the first two drivers in the array.
-
-const returnFirstTwoDrivers = function (drivers) {
-return drivers.slice(0, 2);
+function receivesANamedFunction(callback) {
+  callback();
 }
 
-const returnLastTwoDrivers = function (drivers) {
-  return drivers.slice(-2);
-};
+function returnsANamedFunction() {
+  function namedFunction() {
+    console.log("I am a named function!");
+  }
+  return namedFunction;
+}
 
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-
-const createFareMultiplier = function (multiplier) {
-  return function (fare) {
-    return fare * multiplier;
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I am an anonymous function!");
   };
-};
-
-const fareDoubler = createFareMultiplier(2);
-
-const fareTripler = createFareMultiplier(3);
-
-const selectDifferentDrivers = function (drivers, driverSelector) {
-  return driverSelector(drivers);
-};
+}
